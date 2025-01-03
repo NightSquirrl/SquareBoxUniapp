4c59e0f3a29e711729cd99683db4a489e59cd9a3
nightsoul <w1083670160@163.com>
2025-01-03 21:56:32 +0800

    月工时计算

--- components/WorkingHours/WorkingHours.vue
+++ components/WorkingHours/WorkingHours.vue
@@ -1,18 +1,22 @@
 <template>
 	<view>
 		<view class="uni-container">
-			<uni-card title="高级时间计算器">
+			<!-- 卡片一 -->
+			<uni-card title="月度工时计算">
 				<view class="date-start-end">
 					<text></text>
 					<view class="date-start-end-right">
-						<picker mode="date" :value="startDate" @change="bindStartDate">
-							<view class="uni-input">{{ startDate }}</view>
-						</picker>
-						<uni-icons style="margin-right: 20px" custom-prefix="iconfont" type="icon-rili" size="15"></uni-icons>
-						<picker mode="date" :value="endDate" @change="bindEndDate">
-							<view class="uni-input">{{ endDate }}</view>
-						</picker>
-						<uni-icons custom-prefix="iconfont" type="icon-rili" size="15"></uni-icons>
+						<label for="queryMonth">输入要查询的月份:</label>
+						<input
+							style="width: 30px"
+							name="queryMonth"
+							class="uni-input"
+							type="number"
+							:value="queryMonth"
+							maxlength="2"
+							@input="changeQueryMonth"
+							placeholder="输入查询的月份"
+						/>
 					</view>
 				</view>
 				<uni-table ref="table" border stripe emptyText="暂无更多数据">
@@ -30,31 +34,58 @@
 							<text>{{ getDayOfWeek(item.date) }}</text>
 						</uni-td>
 						<uni-td>
-							<picker mode="time" :value="item.attendanceTime" start="09:01" end="21:01" @change="bindTimeChange(item, $event)">
+							<picker mode="time" :value="item.attendanceTime" start="09:01" end="21:01" @change="bindTimeChange(item, 'attendanceTime', $event)">
 								<view class="uni-input">{{ item.attendanceTime }}</view>
 							</picker>
 						</uni-td>
 						<uni-td>
-							<picker mode="time" :value="item.endAttendance" start="09:01" end="21:01" @change="bindTimeChange(item, $event)">
+							<picker mode="time" :value="item.endAttendance" start="09:01" end="21:01" @change="bindTimeChange(item, 'endAttendance', $event)">
 								<view class="uni-input">{{ item.endAttendance }}</view>
 							</picker>
+						</uni-td>
+					</uni-tr>
+					<uni-tr>
+						<uni-td align="center" colspan="2">
+							<text>本月需工时: {{ totalManHour }}</text>
+							<view>
+								<text>已有工时: {{ hasWorkAllHour }}</text>
+							</view>
+						</uni-td>
+						<uni-td align="center" colspan="2">
+							<view>
+								<text>总工作天数: {{ hasWorkDays }}</text>
+							</view>
+							<view>
+								<text>平均工时: {{ (hasWorkAllHour / hasWorkDays).toFixed(2) }}</text>
+							</view>
 						</uni-td>
 					</uni-tr>
 				</uni-table>
 			</uni-card>
 			<!-- 分页 -->
 			<!-- <view class="uni-pagination-box"><uni-pagination show-icon :page-size="pageSize" :current="pageCurrent" :total="total" @change="change" /></view> -->
+			<!-- 卡片二 -->
 			<uni-card title="设置">
-				<view>
-					<text style="margin-bottom: 10px">每天固定休息时间</text>
-					<input :value="restTime" class="uni-border-input" type="digit" placeholder="带小数点的数字键盘" />
+				<view style="margin-bottom: 10px">
+					<text >每月需要的平均工时</text>
+					<input :value="hoursPerMonth" class="uni-border-input" type="digit" placeholder="请输入数字" />
+				</view>
+				<view style="margin-bottom: 10px">
+					<text >每天休息时间段(不计算工时内)</text>
+					<view class="setting-rest-time" v-for="(restTimeItem, index) in restTimeConfig" :key="index">
+						<picker class="rest-item-time" mode="time" :value="restTimeItem.startTime" start="09:01" end="21:01" @change="bindTimeChange(restTimeItem, 'startTime', $event)">
+							<text>开始时间:{{ restTimeItem.startTime }}</text>
+						</picker>
+						<picker class="rest-item-time" mode="time" :value="restTimeItem.endTime" start="09:01" end="21:01" @change="bindTimeChange(restTimeItem, 'endTime', $event)">
+							<text>结束时间:{{ restTimeItem.endTime }}</text>
+						</picker>
+					</view>
 				</view>
 			</uni-card>
 
 			<uni-card title="结果显示">
 				<view>
 					<text style="margin-bottom: 10px">每天固定休息时间</text>
-					<input :value="restTime" class="uni-border-input" type="digit" placeholder="带小数点的数字键盘" />
 				</view>
 			</uni-card>
 		</view>
@@ -62,13 +93,34 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, watch } from 'vue';
-import { formatDate, generateDateRange, getDayOfWeek, getEndOfWeek, getLastDayOfMonth, getStartOfWeek } from '../../utils/Time';
+import { computed, onMounted, ref, watch } from 'vue';
+import {
+	calculateTimeDifference,
+	formatDate,
+	generateDateRange,
+	getDayOfWeek,
+	getEndOfWeek,
+	getLastDayOfMonth,
+	getStartOfWeek,
+	getWorkingDaysInCurrentMonth,
+	workDayBetweenTwoHours
+} from '../../utils/Time';
 
-function selectionChange() {}
+const queryMonth = ref(new Date().getMonth() + 1);
+const restTimeConfig = ref([
+	{
+		startTime: '00:00',
+		endTime: '00:00'
+	},
+	{
+		startTime: '00:00',
+		endTime: '00:00'
+	}
+]);
+const hoursPerMonth = ref(8);
+
 const startDate = ref(getStartOfWeek());
 const endDate = ref(getEndOfWeek());
-const restTime = ref(0);
 const tableData = ref<
 	{
 		attendanceTime: string;
@@ -77,56 +129,59 @@ const tableData = ref<
 	}[]
 >([]);
 
-function bindTimeChange(row, e) {
-	row.attendanceTime = e.detail.value;
-}
-function bindStartDate(e) {
-	// 开始的时间需要小于结束时间
-	if (new Date(e.detail.value).getTime() - new Date(endDate.value).getTime() < 0) {
-		startDate.value = e.detail.value;
-	}
-}
-function bindEndDate(e) {
-	const currentDate = new Date(e.detail.value);
-	if (currentDate.getTime() - new Date(startDate.value).getTime() > 0) {
-		// 并且时间不允许超过开始时间的月份
-		if (currentDate.getMonth() === new Date(startDate.value).getMonth()) {
-			endDate.value = e.detail.value;
-		} else {
-			currentDate.setMonth(new Date(startDate.value).getMonth());
-			currentDate.setFullYear(new Date(startDate.value).getFullYear());
-			currentDate.setDate(getLastDayOfMonth(startDate.value));
-			endDate.value = formatDate(currentDate.getTime());
-		}
-	}
+function bindTimeChange(row, key, e) {
+	row[key] = e.detail.value;
 }
 
-watch(
-	() => [startDate, endDate],
-	(newData, oldData) => {
-		//  最后修改本次是否存在数据 ,动态生成
-		tableData.value = generateDateRange(startDate.value, endDate.value);
-	},
-	{
-		immediate: true,
-		deep: true
+// 修改当前要查询的月份
+function changeQueryMonth(event) {
+	let inputValue = event.detail.value;
+	// 将输入值限制在 1-12 之间
+	if (inputValue < 1) {
+		inputValue = '1';
+	} else if (inputValue > 12) {
+		inputValue = '12';
 	}
-);
+	queryMonth.value = inputValue;
+}
+
+// 查询该月总工时
+const totalManHour = computed(() => {
+	const tempDate = new Date();
+	tempDate.setMonth(queryMonth.value - 1);
+	return getWorkingDaysInCurrentMonth(tempDate) * hoursPerMonth.value;
+});
+
+//  计算已工作天数
+const hasWorkDays = computed(() => workDayBetweenTwoHours(tableData.value[0]?.date, tableData.value[tableData.value.length - 1]?.date));
+// 计算已打开总工时
+const hasWorkAllHour = computed(() => {
+	let allHors = 0;
+	tableData.value.map((item) => {
+		if (parseInt(item.attendanceTime.split(':')[0]) - parseInt(item.endAttendance.split(':')[0]) < 0) {
+			allHors += calculateTimeDifference(item.attendanceTime, item.endAttendance);
+		}
+	});
+	return allHors;
+});
 
 onMounted(() => {
-	// 初始化结束时间
-	const tempEndDate = startDate.value;
-	const tempEndDateArray = tempEndDate.split('-');
-	tempEndDateArray[2] = getLastDayOfMonth(startDate.value).toString();
-	endDate.value = tempEndDateArray.join('-');
-	uni.setStorage({
-	  key: 'key',
-	  data: 'value',
-	  success: function () {
-	    // 存储成功的逻辑
-		console.log('success');
-	  }
-	});
+	// 初始化数据列标
+	const currentMonth = new Date().getMonth() + 1;
+	if (queryMonth.value === currentMonth) {
+		for (let index = 1; index <= new Date().getDate(); index++) {
+			tableData.value.push({ date: `${new Date().getFullYear()}-${currentMonth.toString().padStart(2, '0')}-${index}`, attendanceTime: '08:00', endAttendance: '00:00' });
+		}
+	}
+
+	// uni.setStorage({
+	// 	key: 'key',
+	// 	data: 'value',
+	// 	success: function () {
+	// 		// 存储成功的逻辑
+	// 		console.log('success');
+	// 	}
+	// });
 });
 </script>
 
@@ -166,5 +221,18 @@ onMounted(() => {
 	height: 40px;
 	border-radius: 10px;
 	padding-left: 20px;
+}
+
+// 设置休息时间
+.setting-rest-time {
+	display: flex;
+	flex-direction: row;
+	margin-left: 20px;
+	.rest-item-time {
+		height: 40px;
+		margin-right:20px;
+		display: flex;
+		align-items: center;
+	}
 }
 </style>
--- utils/Time.ts
+++ utils/Time.ts
@@ -1,3 +1,4 @@
+const holidays = ['2025-01-01', '2025-01-26']; // 假日数组
 export function getStartOfWeek(date = new Date()) {
 	const currentDate = new Date(date);
 	const dayOfWeek = currentDate.getDay();
@@ -57,20 +58,85 @@ export function generateDateRange(startDate, endDate) {
 
 /** 获取当前日期的最后一天 */
 export function getLastDayOfMonth(dateString) {
-    const date = new Date(dateString);
-    // 设置为下个月的第0天，就会返回本月最后一天
-    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+	const date = new Date(dateString);
+	// 设置为下个月的第0天，就会返回本月最后一天
+	return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
 
 export function formatDate(timestamp) {
-    const date = new Date(timestamp);
-    
-    // 获取年月日
-    const year = date.getFullYear();
-    // getMonth() 返回 0-11，需要加1
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    
-    // 拼接成 YYYY-MM-DD 格式
-    return `${year}-${month}-${day}`;
+	const date = new Date(timestamp);
+
+	// 获取年月日
+	const year = date.getFullYear();
+	// getMonth() 返回 0-11，需要加1
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+
+	// 拼接成 YYYY-MM-DD 格式
+	return `${year}-${month}-${day}`;
+}
+
+// 获取本月有多少个工作日
+export function getWorkingDaysInCurrentMonth(date = new Date()) {
+	const time = date; // 当前日期
+	const year = time.getFullYear(); // 当前年份
+	const month = time.getMonth(); // 当前月份 (0-11)
+	const daysInMonth = new Date(year, month + 1, 0).getDate(); // 当前月的总天数
+
+	let workingDays = 0;
+
+	for (let day = 1; day <= daysInMonth; day++) {
+		const date = new Date(year, month, day);
+		const dayOfWeek = date.getDay(); // 获取星期几 (0-6, 0 表示星期日)
+		const formattedDate = date.toISOString().split('T')[0];
+		// 如果不是周六(6)或周日(0)，则是工作日
+		if (dayOfWeek !== 0 && dayOfWeek !== 6 && !holidays.includes(formattedDate)) {
+			workingDays++;
+		}
+	}
+
+	return workingDays;
+}
+
+//  获取两个时间的工作日
+export function workDayBetweenTwoHours(startTime, endTime) {
+	let startDate = new Date(startTime);
+	let endDate = new Date(endTime);
+	let hasWorkingDays = 0;
+	if (startDate > endDate) {
+		const tempDate = startDate;
+		startDate = endDate;
+		endDate = tempDate;
+	}
+
+	for (let day = 1; day <= endDate.getDate(); day++) {
+		const date = new Date(startDate.getFullYear(), startDate.getMonth(), day);
+		const dayOfWeek = date.getDay(); // 获取星期几 (0-6, 0 表示星期日)
+		const formattedDate = date.toISOString().split('T')[0];
+		// 如果不是周六(6)或周日(0)，则是工作日
+		if (dayOfWeek !== 0 && dayOfWeek !== 6 && !holidays.includes(formattedDate)) {
+			hasWorkingDays++;
+		}
+	}
+
+	return hasWorkingDays;
+}
+
+// 获取两个时间之间多少小时
+export  function calculateTimeDifference(time1, time2) {
+	// 将时间字符串解析为小时和分钟
+	const [hour1, minute1] = time1.split(':').map(Number);
+	const [hour2, minute2] = time2.split(':').map(Number);
+
+	// 将时间转换为分钟
+	const totalMinutes1 = hour1 * 60 + minute1;
+	const totalMinutes2 = hour2 * 60 + minute2;
+
+	// 计算分钟差
+	const differenceInMinutes = Math.abs(totalMinutes1 - totalMinutes2);
+
+	// 转换为小时并四舍五入保留两位小数
+	const differenceInHours = (differenceInMinutes / 60).toFixed(2);
+
+	return parseFloat(differenceInHours);
 }
